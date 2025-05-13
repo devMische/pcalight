13edd55d4c1685e19e4e4eed49327471a22013ae
DerMische <ehcsimred@web.de>
2025-05-13 21:24:27 +0200

    addon updater implemented

--- __init__.py
+++ __init__.py
@@ -17,6 +17,7 @@
 # ##### END GPL LICENSE BLOCK #####
 
 import bpy
+from . import addon_updater_ops
 
 from . pcal_settings import PCALsettings
 from . pcal_ui import PCAL_PT_main, PCAL_PT_camera
@@ -38,12 +39,75 @@ bl_info = {
 }
 
 
+@addon_updater_ops.make_annotations
+class PCALPreferences(bpy.types.AddonPreferences):
+    """ preferences"""
+    bl_idname = __package__
+
+    # Addon updater preferences.
+
+    auto_check_update = bpy.props.BoolProperty(
+        name="Auto-check for Update",
+        description="If enabled, auto-check for updates using an interval",
+        default=False)
+
+    updater_interval_months = bpy.props.IntProperty(
+        name='Months',
+        description="Number of months between checking for updates",
+        default=0,
+        min=0)
+
+    updater_interval_days = bpy.props.IntProperty(
+        name='Days',
+        description="Number of days between checking for updates",
+        default=7,
+        min=0,
+        max=31)
+
+    updater_interval_hours = bpy.props.IntProperty(
+        name='Hours',
+        description="Number of hours between checking for updates",
+        default=0,
+        min=0,
+        max=23)
+
+    updater_interval_minutes = bpy.props.IntProperty(
+        name='Minutes',
+        description="Number of minutes between checking for updates",
+        default=0,
+        min=0,
+        max=59)
+
+    def draw(self, context):
+        layout = self.layout
+
+        # Works best if a column, or even just self.layout.
+        mainrow = layout.row()
+        col = mainrow.column()
+
+        # Updater draw function, could also pass in col as third arg.
+        addon_updater_ops.update_settings_ui(self, context)
+
+        # Alternate draw function, which is more condensed and can be
+        # placed within an existing draw function. Only contains:
+        #   1) check for update/update now buttons
+        #   2) toggle for auto-check (interval will be equal to what is set above)
+        # addon_updater_ops.update_settings_ui_condensed(self, context, col)
+
+        # Adding another column to help show the above condensed ui as one column
+        # col = mainrow.column()
+        # col.scale_y = 2
+        # ops = col.operator("wm.url_open","Open webpage ")
+        # ops.url=addon_updater_ops.updater.website
+
+
 classes = (PCALsettings,
            PCAL_PT_main,
            PCAL_PT_camera,
            PCAL_OT_addpcop,
            PCAL_OT_addhsop,
-           PCAL_OT_look360cam)
+           PCAL_OT_look360cam,
+           PCALPreferences)
 
 
 register, unregister = bpy.utils.register_classes_factory(classes)
