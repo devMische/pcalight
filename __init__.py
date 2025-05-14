331df86cf1250624b704b5ee73576d37a5b88f44
DerMische <ehcsimred@web.de>
2025-05-14 09:04:00 +0200

    v1.01 just for testing the updater

--- __init__.py
+++ __init__.py
@@ -29,7 +29,7 @@ from . pcal_op_camviews import PCAL_OT_look360cam
 bl_info = {
     "name": "PanoCamAdder Light",
     "author": "DerMische",
-    "version": (1, 0, 0),
+    "version": (1, 0, 1),
     "blender": (4, 3, 0),
     "location": "View3D > UI > PCA light",
     "description": "Free version of the PanoCamAdder",
